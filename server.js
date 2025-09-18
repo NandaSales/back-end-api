c26d7cb9ab8c7521925a94288351c310d236cae8
NandaSales <frs17@aluno.ifnmg.edu.br>
2025-09-18 18:09:35 +0000

    oi

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+const db = new Pool({
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
+
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+app.get("/", async (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota GET / solicitada");
   res.json({
-		message: "API para _____",      // Substitua pelo conteúdo da sua API
-    author: "Seu_nome_completo",    // Substitua pelo seu nome
+    message: "API para ____",
+    author: "____",
+    statusBD: dbStatus   // Acrescente esta linha
   });
+
 });
+
+
+
+
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço rodando na porta:  ${port}`);
